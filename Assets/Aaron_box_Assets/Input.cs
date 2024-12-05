d18cd36f5dfe48b052649461fac4fa0a2b140e85
Aaron C <1031808@student.sae.edu.au>
2024-12-06 06:15:50 +0800

    my scene added

--- Assets/Aaron_box_Assets/Input.cs
+++ Assets/Aaron_box_Assets/Input.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Input : MonoBehaviour
 {
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("test");
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            Debug.Log("hello");
+        }
         
+
+
+
     }
 }
