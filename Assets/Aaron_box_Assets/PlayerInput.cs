61a520b60db93803b8a94de39bd367729b730de7
Aaron C <1031808@student.sae.edu.au>
2024-12-10 11:45:53 +0800

    1/2 of scream game done

--- Assets/Aaron_box_Assets/PlayerInput.cs
+++ Assets/Aaron_box_Assets/PlayerInput.cs
@@ -1,15 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using TMPro;
 
 public class PlayerInput : MonoBehaviour
 {
-    
+    public int count = 100;
+   
+    public UnityEngine.UI.Slider ProgressBar;
+
+    private float SoundPitch = 1.0f;
+    public AudioSource Sound;
+
+    public TextMeshProUGUI Text;
+    private Vector3 initialPosition;
+    private Vector3 directionOfShake;
+    public float amplitude = 1.0f;
+    public float frequency = 1.0f; 
 
     // Start is called before the first frame update
     void Start()
     {
+        Vector3 directionOfShake = transform.forward;
+        ProgressBar.value = count;
 
+        initialPosition = Text.transform.position;
+}
+    void FixedUpdate()
+    {
+        Text.transform.position = initialPosition + directionOfShake * (-amplitude + Mathf.PingPong(frequency * Time.fixedDeltaTime, 2f * amplitude));
     }
 
     // Update is called once per frame
@@ -17,11 +38,28 @@ public class PlayerInput : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) == true)
         {
-            Debug.Log("hello");
+            Pressed();
         }
-        
+
+    }
+
+    public void Pressed()
+    {
+
+        count = count - 1;
+        ProgressBar.value = count;
+
+
+        SoundPitch = SoundPitch + 0.04f;
+        Sound.pitch = SoundPitch;
+        Sound.Play();
+      
+
 
 
 
     }
+
+
+
 }
